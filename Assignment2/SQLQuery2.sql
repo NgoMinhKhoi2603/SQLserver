00cf54205890b012814121783a82f808eb29bca6
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2022-10-14 12:29:23 +0700

    Create SQLQuery2.sql

---
+++ Assignment2/SQLQuery2.sql
@@ -0,0 +1,25 @@
+create table Customers(
+	Id int primary key identity(1,1),
+	Name nvarchar(255) not null,
+	Address nvarchar(255) not null,
+	PhoneNumber int unique not null
+);
+
+create table Orders(
+	Code int primary key,
+	Date date not null check(Date <= getdate()),
+	CustomerId int foreign key references Customers(Id)
+);
+
+create table Goods(
+	Id int primary key identity(1,1),
+	Name nvarchar(255) not null,
+	Description ntext not null,
+	Unit nvarchar(255) not null,
+	Price int not null
+);
+
+create table OrderGood(
+	OrderCode int foreign key references Orders(Code),
+	GoodId int foreign key references Goods(Id)
+);
